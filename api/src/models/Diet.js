2234dd7e6a959bddcc56e3ad77d45f4192b47267
Jose <mmadrizjose@gmail.com>
2022-08-02 19:58:59 -0300

    Change: Removed timestamps from Models and created Model associations

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -3,10 +3,16 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = sequelize => {
   // defino el modelo
-  sequelize.define('diet', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    'diet',
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,29 +3,35 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = sequelize => {
   // defino el modelo
-  sequelize.define('recipe', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      allowNull: false,
+  sequelize.define(
+    'recipe',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      summary: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      healthScore: {
+        type: DataTypes.INTEGER,
+      },
+      steps: {
+        type: DataTypes.TEXT,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    summary: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    healthScore: {
-      type: DataTypes.INTEGER,
-    },
-    steps: {
-      type: DataTypes.TEXT,
-    },
-    image: {
-      type: DataTypes.STRING,
-    },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
