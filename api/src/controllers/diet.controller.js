37eb44b1f563fb87865b88601bcaad4e863fbb74
Jose <mmadrizjose@gmail.com>
2022-08-03 16:20:11 -0300

    Feat: First working diet controller

--- api/src/controllers/diet.controller.js
+++ api/src/controllers/diet.controller.js
@@ -1,30 +1,38 @@
 require('dotenv').config();
+const axios = require('axios');
 const { Diet } = require('../db.js');
+const { API_KEY } = process.env;
 
 const getAllDiets = async (req, res, next) => {
   try {
-    const dietTypes = [
-      'dairy free',
-      'fodmap friendly',
-      'gluten free',
-      'ketogenic',
-      'lacto ovo vegetarian',
-      'lacto vegetarian',
-      'ovo vegetarian',
-      'paleolithic',
-      'pescatarian',
-      'primal',
-      'vegan',
-      'vegetarian',
-      'whole 30',
-    ];
-    
-    dietTypes.forEach(async diet => await Diet.findOrCreate({ where: { name: diet } }));
+    // Recipes contain diet types
+    const recipes = await axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=50`
+    );
 
-    const diets = await Diet.findAll();
-    res.status(201).json(diets)
+    // Parsing the diet types into an iterable
+    const dietsFromRecipes = new Set(
+      recipes.data.results.map(recipe => recipe.diets).flat(1)
+    );
+    const dietTypes = [...dietsFromRecipes];
+    
+    // Finding or creating each diet type
+    const diets = dietTypes.map(dietType => {
+      return new Promise(async (resolve, reject) => {
+        const dietsCreation = await Diet.findOrCreate({
+          where: { name: dietType },
+        });
+        resolve(dietsCreation);
+        reject(error => next(error));
+      });
+    });
+    await Promise.all(diets);
+
+    // Sending back all the diet types
+    const dbDiets = await Diet.findAll();
+    res.status(201).json(dbDiets);
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
