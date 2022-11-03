626b7ef64c5d2bb9c2dcb1bfe14fe78152fe267b
José Madriz <92412460+madrizjosea@users.noreply.github.com>
2022-11-03 16:20:55 -0300

    [FIX] title and name change

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -5,7 +5,7 @@ import s from './Landing.module.css';
 function Landing() {
   return (
     <div className={s.container}>
-      <h1 className={s.header}>Food Guide</h1>
+      <h1 className={s.header}>Recipe Guide</h1>
       <p>find inspiration in our world wide collection of recipes</p>
       <button className={s.btn}>
         <Link to="/home">Get Started</Link>
